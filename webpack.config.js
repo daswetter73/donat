c882252dba7e53fcda6595ca9182909b3bb2ef3d
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-02-03 10:47:51 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    open: true,
+    port: 9001
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                "targets": "defaults" 
+              }],
+              '@babel/preset-react'
+            ]
+          }
+        }]
+      }
+    ]
+  },
+  resolve: {
+    // modules: [process.env.NODE_PATH],
+    extensions: [".js"],
+    fallback: {
+      // buffer: require.resolve("buffer/"),
+      http: false,
+      url: false,
+      stream: false,
+      crypto: false,
+      https: false,
+      net: false,
+      tls: false,
+      zlib: false,
+      os: false,
+      path: false,
+      fs: false,
+      readline: false,
+      child_process: false,
+    },
+  },
+}
+
+
