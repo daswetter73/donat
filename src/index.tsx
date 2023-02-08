8a999f5f370a9492fc48c49b7de79ff11b70f8e4
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-02-08 10:49:19 +0300

    added eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
+import { type ReactElement, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { useEffect, useState } from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!)
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 declare global {
-  interface  Window {
+  interface Window {
     offchain: any;
   }
 }
-const App = () => {
-  const [offchain, setOffchain] = useState<Window["offchain"]>()
-  useEffect(()=>{
-    window.offchain.then(setOffchain)
-  },[])
+const App = (): ReactElement => {
+  const [offchain, setOffchain] = useState<Window['offchain']>();
+  useEffect(() => {
+    window.offchain.then(setOffchain);
+  }, []);
 
   return (
     <div>
       <h1>Offchain integration</h1>
-      <button onClick={()=>offchain?.getProtocolInfo(console.log)}>Connect wallet</button>
+      <button onClick={() => offchain?.getProtocolInfo(console.log)}>
+        Connect wallet
+      </button>
     </div>
-  )
-}
+  );
+};
 
-root.render(<App/>)
+root.render(<App />);
